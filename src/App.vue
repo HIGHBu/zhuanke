d67b0a706edf69e67f4d79a8263ff6a18b809b6d
Diego Brando <3190103302@zju.edu.cn>
2022-10-21 12:18:12 +0800

    增加对于人物距离的判断

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,26 @@
   const galleryScale = 20
   const characterScale = 0.2
   const position = ref({x: 0, y: 0, z: 0})
-
+  const visibleDistance = 500
+  const initPosition = ref({x: 0, y: 200, z: 0})
+  var persons = [
+    {
+      x: 0,
+      y: 0,
+      z: 0,
+      animate: "idle",
+      src: "basic",
+      scale: 0.2,
+    },
+    {
+      x: 200,
+      y: 0,
+      z: 0,
+      animate: "walking",
+      src: "basic",
+      scale: 0.2,
+    },
+  ]
   const handleClick = (ev:types.MouseEvent) => {
     if (ev.distance >= 200) {
       let model=characterRef.value
@@ -22,9 +41,16 @@
   }
 </script>
 
+<script lang="ts">
+  export default {
+    computed: {
+    },
+  };
+</script>
+
 <template>
-  <Test></Test>
-  <!-- <World>
+  <!-- <Test></Test> -->
+  <World>
     <Model
       name="gallery"
       src="gallery_0925.glb"
@@ -41,7 +67,7 @@
       :min-polar-angle="90" 
       :max-polar-angle="110"
     >
-      <Model
+      <!-- <Model
         name="character"
         src="c.glb"
         physics="character"
@@ -53,16 +79,16 @@
         :look-to="[position.x, undefined, position.z, 0.1]"
         :move-to="[position.x, undefined, position.z, 5]"
         @move-to-end="handleMoveToEnd"
-      />
+      /> -->
 
       <Model
         name="character"
         src="basic/src.glb"
         physics="character"
         ref="characterRef"
-        :x="0"
-        :y="200"
-        :z="0"
+        :x="initPosition.x"
+        :y="initPosition.y"
+        :z="initPosition.z"
         :scale="characterScale"
         :animations="{ idle : 'basic/idle.glb', walking: 'basic/walk.glb' }"
         :animation="characterPose"
@@ -71,6 +97,21 @@
         @move-to-end="handleMoveToEnd"
       />
     </ThirdPersonCamera>
+    <Model
+      v-for="(person, id) in persons.filter(p=> {
+        if (characterRef) {
+          return  Math.sqrt(Math.pow((p.x - characterRef.x), 2) + Math.pow((p.y - characterRef!.y), 2) + Math.pow((p.z - characterRef!.z), 2)) < visibleDistance
+        }
+        return  Math.sqrt(Math.pow((p.x - initPosition.x), 2) + Math.pow((p.y - initPosition.y), 2) + Math.pow((p.z - initPosition.z), 2)) < visibleDistance
+      })"
+      :src="person.src + '/src.glb'"
+      :scale="person.scale"
+      :x="person.x"
+      :y="person.y"
+      :z="person.z"
+      :animations="{ idle : person.src + '/idle.glb', walking: person.src + '/walk.glb' }"
+      :animation="person.animate"
+    />
     <SvgMesh
       src="arrow.svg"
       :metalness-factor="1"
@@ -85,5 +126,5 @@
       :z="position.z"
       :animation="{rotationY: [0, 45, 90, 135, 180, 225, 270, 315, 360]}"
     />
-  </World> -->
+  </World>
 </template>
